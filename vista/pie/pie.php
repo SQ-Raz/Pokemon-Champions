c32ec1d1728b60fddd8d65d4da10baf25c0bd791
Linda Kate Lovera Fernandez <linkat3@gmail.com>
2024-10-31 11:10:15 +0000

    subiendo miPokedex

---
+++ vista/pie/pie.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../pie/pie.css">
+<footer id="pie">
+    <span class="copyright">&copy; 2024 </span>
+    <a href="https://www.cpokemon.com/">Atrapalos a todos!!</a>
+    <div id="redes">
+        <img class="logo" src="../logos/facebook-transparente.webp" alt="Facebook">
+        <img class="logo" src="../logos/new-twitter-icon-x-png.webp" alt="Social X">
+        <img class="logo" src="../logos/vimeo.webp" alt="Vimeo">
+    </div>
+</footer>
