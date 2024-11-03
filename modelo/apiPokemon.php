26e71b66fe9784b71a412555c0cb4d707718babf
Linda Kate Lovera Fernandez <132331448+linkat3@users.noreply.github.com>
2024-11-03 19:03:22 +0000

    Update apiPokemon.php

--- modelo/apiPokemon.php
+++ modelo/apiPokemon.php
@@ -5,10 +5,15 @@
 
         function __construct($pokemon)
         {
+            //Mostrar el spinner 
+           document.getElementById('spinner').style.display = 'block'; 
             $api = curl_init("https://pokeapi.co/api/v2/pokemon/".$pokemon);
             curl_setopt($api,CURLOPT_RETURNTRANSFER,true);
             $this->datos = json_decode(curl_exec($api));
             curl_close($api);
+            //ocultar spinner
+              document.getElementById('spinner').style.display = 'none'; 
+
         } 
     }
 ?>
