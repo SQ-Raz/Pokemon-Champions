7acddceda846162ec1b12de372798eda460faeb2
SQ-Raz <razukmamo@gmail.com>
2024-11-04 13:49:04 +0000

    Primer commit de la rama razuk

--- vista/encabezado/encabezado.php
+++ vista/encabezado/encabezado.php
@@ -5,9 +5,9 @@
     </div>
     <nav>
         <ul>
-            <li><a href="./principal.php">Inicio</a></li>
-            <li><a href="#">Acerca de</a></li>
-            <li><a href="#">Contacto</a></li>
+            <li><a href="../principal/principal.php">Inicio</a></li>
+            <li><a href="../Acerca/acerca.php">Acerca de</a></li>
+            <li><a href="../contacto/contactanos.php">Contacto</a></li>
         </ul>
     </nav>
 </header>
